f307454dd2dede0c9fca9c6b26d22748d29e4dc5
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-03-27 15:06:37 +0530

    addedSummary

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meal";
 
 function App() {
   return (
     <div>
       <Header/>
+      <Meals/>
     </div>
   );
 }
---
+++ Components/Meals/Meal.js
@@ -0,0 +1,9 @@
+import { Fragment } from "react"
+import MealsSummary from "./MealsSummarry"
+
+const Meals = ()=>{
+    return <Fragment>
+        <MealsSummary/>
+    </Fragment>
+}
+export default Meals
