159ebc058402707cea47660f48b35328865135ce
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-03-27 16:19:41 +0530

    addingList

--- Components/Meals/Meal.js
+++ Components/Meals/Meal.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react"
+import MealsList from "./MealsList"
 import MealsSummary from "./MealsSummarry"
 
 const Meals = ()=>{
     return <Fragment>
         <MealsSummary/>
+        <MealsList/>
     </Fragment>
 }
 export default Meals
